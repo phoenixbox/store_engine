f3a9122b7226a5dd2332a3c974d66e907d39fb77
Blair Anderson <blair81@gmail.com>
2013-03-31 16:51:56 -0600

    product partials and category views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def logo
+    link_to root_path, class:"logo pull-left" do
+      image_tag('http://www.honeycando.com/images/hcd_logo.gif')
+    end   
+  end
+  
 end
