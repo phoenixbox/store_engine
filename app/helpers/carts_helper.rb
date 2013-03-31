83df812f136624530347cbf51e6ead7b39f400fa
Blair Anderson <blair81@gmail.com>
2013-03-31 11:34:10 -0600

    started to put designmodo flat-free to use

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -3,7 +3,7 @@ module CartsHelper
   def subtotal(shopping_cart)
     result = shopping_cart.collect do |product, count|
       product.price.to_f * count
-    end.inject(:+)
-    "%.2f" % (result / 100.00)
+    end.inject(:+) || 0
+    "%.2f" % (result / 100.0)
   end
 end
