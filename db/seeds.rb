ab4d808e41dadee6423939792bcada23787ef7ba
Blair Anderson <blair81@gmail.com>
2013-03-31 13:09:12 -0600

    updating seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,79 @@ jeff = User.create!(username: "Jeff Casimir", display_name:"j3",email:"demoXX+je
 
 steve = User.create!(username:"Steve Klabnik", display_name:"SkrilleX", email:"demoXX+steve@jumpstartlab.com", password:"password", password_confirmation:"password", admin:1 )
 
+soft_storage = Category.find_by_name("soft-storage")
+soft_storage.update_attributes(photo_url:"")
+
+hangers = Category.find_by_name("hangers")
+hangers.update_attributes(photo_url:"")
+
+ironing_board_covers = Category.find_by_name("ironing-board-covers")
+ironing_board_covers.update_attributes(photo_url:"")
+
+kitchen_accessories = Category.find_by_name("kitchen-accessories")
+kitchen_accessories.update_attributes(photo_url:"")
+
+hampers = Category.find_by_name("hampers")
+hampers.update_attributes(photo_url:"")
+
+shelves = Category.find_by_name("shelves")
+shelves.update_attributes(photo_url:"")
+
+shelf = Category.find_by_name("shelf")
+shelf.update_attributes(photo_url:"")
+
+shelving = Category.find_by_name("shelving")
+shelving.update_attributes(photo_url:"")
+
+storage_accessories = Category.find_by_name("storage-accessories")
+storage_accessories.update_attributes(photo_url:"")
+
+trash_cans = Category.find_by_name("trash-cans")
+trash_cans.update_attributes(photo_url:"")
+
+carts = Category.find_by_name("carts")
+carts.update_attributes(photo_url:"")
+
+garment_racks = Category.find_by_name("garment-racks")
+garment_racks.update_attributes(photo_url:"")
+
+ironing_boards = Category.find_by_name("ironing-boards")
+ironing_boards.update_attributes(photo_url:"")
+
+laundry_bag = Category.find_by_name("laundry-bag")
+laundry_bag.update_attributes(photo_url:"")
+
+laundry = Category.find_by_name("laundry")
+laundry.update_attributes(photo_url:"")
+
+drying = Category.find_by_name("drying")
+drying.update_attributes(photo_url:"")
+
+shoe_storage = Category.find_by_name("shoe-storage")
+shoe_storage.update_attributes(photo_url:"")
+
+laundry_sorters = Category.find_by_name("laundry-sorters")
+laundry_sorters.update_attributes(photo_url:"")
+
+laundry_centers = Category.find_by_name("laundry-centers")
+laundry_centers.update_attributes(photo_url:"")
+
+vacuum_storage = Category.find_by_name("vacuum-storage")
+vacuum_storage.update_attributes(photo_url:"")
+
+wardrobe = Category.find_by_name("wardrobe")
+wardrobe.update_attributes(photo_url:"")
+
+closets = Category.find_by_name("closets")
+closets.update_attributes(photo_url:"")
+
+tv_mounting_brackets  = Category.find_by_name("tv-mounting-brackets")
+tv_mounting_brackets .update_attributes(photo_url:"")
+
+laundry_for_dummies = Category.find_by_name("laundry-for-dummies")
+laundry_for_dummies.update_attributes(photo_url:"")
+
+
 
 
 #At least 10 sample orders, with at least two at each stage of fulfillment (pending, shipped, etc)
