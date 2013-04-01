c9537c49ddabb64493748fe434eb0af0f1c70b36
Blair Anderson <blair81@gmail.com>
2013-03-31 20:39:11 -0600

    cleanup on aisle SEED

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,80 +17,10 @@ frank = User.create!(username: "Franklin Webber",email:"demoXX+franklin@jumpstar
 jeff = User.create!(username: "Jeff Casimir", display_name:"j3",email:"demoXX+jeff@jumpstartlab.com", password:"password", password_confirmation:"password", admin:0)
 
 steve = User.create!(username:"Steve Klabnik", display_name:"SkrilleX", email:"demoXX+steve@jumpstartlab.com", password:"password", password_confirmation:"password", admin:1 )
+
 file = File.new('./doc/products_for_store_engine.csv')
 Product.import(file)
 
-soft_storage = Category.find_by_name("soft-storage")
-soft_storage.update_attributes(photo_url:soft_storage.products.first.photo_url)
-
-hangers = Category.find_by_name("hangers")
-hangers.update_attributes(photo_url:hangers.products.first.photo_url)
-
-ironing_board_covers = Category.find_by_name("ironing-board-covers")
-ironing_board_covers.update_attributes(photo_url:ironing_board_covers.products.first.photo_url)
-
-kitchen_accessories = Category.find_by_name("kitchen-accessories")
-kitchen_accessories.update_attributes(photo_url:kitchen_accessories.products.first.photo_url)
-
-hampers = Category.find_by_name("hampers")
-hampers.update_attributes(photo_url:hampers.products.first.photo_url)
-
-shelves = Category.find_by_name("shelves")
-shelves.update_attributes(photo_url:shelves.products.first.photo_url)
-
-shelf = Category.find_by_name("shelf")
-shelf.update_attributes(photo_url:shelf.products.first.photo_url)
-
-shelving = Category.find_by_name("shelving")
-shelving.update_attributes(photo_url:shelving.products.first.photo_url)
-
-storage_accessories = Category.find_by_name("storage-accessories")
-storage_accessories.update_attributes(photo_url:storage_accessories.products.first.photo_url)
-
-trash_cans = Category.find_by_name("trash-cans")
-trash_cans.update_attributes(photo_url:trash_cans.products.first.photo_url)
-
-carts = Category.find_by_name("carts")
-carts.update_attributes(photo_url:carts.products.first.photo_url)
-
-garment_racks = Category.find_by_name("garment-racks")
-garment_racks.update_attributes(photo_url:garment_racks.products.first.photo_url)
-
-ironing_boards = Category.find_by_name("ironing-boards")
-ironing_boards.update_attributes(photo_url:ironing_boards.products.first.photo_url)
-
-laundry_bag = Category.find_by_name("laundry-bag")
-laundry_bag.update_attributes(photo_url:laundry_bag.products.first.photo_url)
-
-laundry = Category.find_by_name("laundry")
-laundry.update_attributes(photo_url:laundry.products.first.photo_url)
-
-drying = Category.find_by_name("drying")
-drying.update_attributes(photo_url:"https://s3.amazonaws.com/store_engine/small/DRY-01107_450px.jpg")
-
-shoe_storage = Category.find_by_name("shoe-storage")
-shoe_storage.update_attributes(photo_url:"https://s3.amazonaws.com/store_engine/small/SHO-01171_450px.jpg")
-
-laundry_sorters = Category.find_by_name("laundry-sorters")
-laundry_sorters.update_attributes(photo_url:"https://s3.amazonaws.com/store_engine/small/SRT-01196_450px.jpg")
-
-laundry_centers = Category.find_by_name("laundry-centers")
-laundry_centers.update_attributes(photo_url:"https://s3.amazonaws.com/store_engine/small/SRT-01235_450px.jpg")
-
-vacuum_storage = Category.find_by_name("vacuum-storage")
-vacuum_storage.update_attributes(photo_url:"https://s3.amazonaws.com/store_engine/small/VAC-01378_450px.jpg")
-
-wardrobe = Category.find_by_name("wardrobe")
-wardrobe.update_attributes(photo_url:"https://s3.amazonaws.com/store_engine/small/WRD-02124_450px.jpg")
-
-closets = Category.find_by_name("closets")
-closets.update_attributes(photo_url:"https://s3.amazonaws.com/store_engine/small/WRD-01272_450px.jpg")
-
-tv_mounting_brackets  = Category.find_by_name("tv-mounting-brackets")
-tv_mounting_brackets .update_attributes(photo_url:"https://s3.amazonaws.com/store_engine/small/BRK-01508_450px.jpg")
-
-laundry_for_dummies = Category.find_by_name("laundry-for-dummies")
-laundry_for_dummies.update_attributes(photo_url:"https://s3.amazonaws.com/store_engine/small/LDY-01427_450px.jpg")
 
 
 
