bfe01b23a3b9ec47537e4576d270afc17c740b9f
Blair Anderson <blair81@gmail.com>
2013-04-01 18:45:58 -0600

    design is getting closer, moved some stuff around. still a work in progress

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,12 @@
 class ProductsController < ApplicationController
   
   def index
-    @products = Product.where(:visible => true).shuffle[0..20]
+    if params[:q]
+      @products = Product.where(:visible => true).search(params[:q])
+    else
+      @products = Product.where(:visible => true).shuffle[0..20]
+    end
+
   end
 
   def show
--- app/models/product.rb
+++ app/models/product.rb
@@ -26,4 +26,13 @@ class Product < ActiveRecord::Base
     end
   end
 
+  def self.search(search)
+  if search
+    find(:all, :conditions => ['title LIKE ?', "%#{search}%"])
+  else
+    find(:all)
+  end
+
+end
+
 end
